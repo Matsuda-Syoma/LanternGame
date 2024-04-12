eaf4ebc91d022937883ac8fc12e91b8ab8ac6f2f
Matsuda-Syoma <122336837+Matsuda-Syoma@users.noreply.github.com>
2024-04-12 10:16:57 +0900

    爆発を早くした

--- LanturnGame/Explosion.cpp
+++ LanturnGame/Explosion.cpp
@@ -12,17 +12,17 @@ Explosion::~Explosion()
 void Explosion::Update()
 {
 	if (flg) {
-		if (count > 90) {
+		if (count > 45) {
 			flg = false;
 		}
 		count++;
-		radius = count;
+		radius = count * 2;
 	}
 }
 
 void Explosion::Draw() const
 {
-	DrawCircle(location.x, location.y, count, 0xff4444, true, true);
+	DrawCircle(location.x, location.y, radius, 0xff4444, true, true);
 }
 
 bool Explosion::Getflg() const
