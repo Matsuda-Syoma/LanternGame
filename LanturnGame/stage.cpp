2fe2ae41296a459c0d0575fed49a8ed0e17b92bd
nakamurakouki4 <122337032+nakamurakouki4@users.noreply.github.com>
2024-04-16 11:05:03 +0900

    範囲(仮)

---
+++ LanturnGame/stage.cpp
@@ -0,0 +1,19 @@
+#include"stage.h"
+#include"DxLib.h"
+
+stage::stage() {
+
+	
+}
+
+stage::~stage() {
+	
+}
+
+void Stage::Update() {
+
+}
+
+void Stage::Draw() const {
+	int DrawBox(int x1, int y1, int x2, int y2, unsigned int Color, int FillFlag);
+}
