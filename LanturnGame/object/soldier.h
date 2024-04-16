8772a3d50bf4ee6d2fa66e4be731916c6f938f3a
Your Name <aito-maezato-k22f@stu.kbc.ac.jp>
2024-04-16 11:09:29 +0900

    兵隊の描画と移動の処理(未完成)

--- LanturnGame/object/soldier.h
+++ LanturnGame/object/soldier.h
@@ -1,15 +1,28 @@
 #pragma once
 #include "../CharaBase.h"
+#include "../Player.h"
 
 class soldier : public CharaBase
 {
 public:
 	soldier();
 	~soldier();
+
 	//初期化
 	void Initialize();
+	//終了時
+	void finalize();
 	//更新
 	void Upadate();
+	//描画
 	void Draw();
+
+	//移動
+	void Move();
+
+private:
+	Player* player;
+	int image;	//画像イメージ
+	Vector2D PL;
 };
 
