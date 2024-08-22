da4435aad0b0f271d3975fdf58204082de2ec2c4
Your Name <aito-maezato-k22f@stu.kbc.ac.jp>
2024-08-22 15:31:02 +0900

    説明完成(仮)

--- LanturnGame/Description.cpp
+++ LanturnGame/Description.cpp
@@ -4,7 +4,7 @@
 #include "DxLib.h"
 
 #define PI 3.1415926535897932384626433832795f
-#define thousand 1000
+#define thousand 2000
 
 Description::Description()
 {
@@ -33,38 +33,37 @@ Description::~Description()
 
 void Description::Update()
 {
-	if (InputControl::GetButtonDown(XINPUT_BUTTON_DPAD_RIGHT))
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_A))
 	{
-		if(MovepngMax < 5000)
+		if(MovepngMax < 6000)
 		MovepngMax += thousand;
 	}
-	if (InputControl::GetButtonDown(XINPUT_BUTTON_DPAD_LEFT))
+	/*if (InputControl::GetButtonDown(XINPUT_BUTTON_A))
 	{
 		if (MovepngMax > 0)
 		MovepngMax -= thousand;
-	}
+	}*/
 	if (Movepng != MovepngMax)
 	{
 		if (Movepng < MovepngMax)
 		{
-			Movepng += 50;
+			Movepng += 100;
 		}
-		if (Movepng > MovepngMax)
+		/*if (Movepng > MovepngMax)
 		{
-			Movepng -= 50;
-		}
+			Movepng -= 100;
+		}*/
 	}
 }
 
 void Description::Draw()
 {
-	DrawBox(220, 10, 1060, 340, 0xffffff, true);
+	DrawBox(10, 10, 1270, 340, 0xffffff, true);
 
 	//‰æ‘œ‚Ì•`‰æ
-	DrawRotaGraph((640 - (thousand * 0)) + Movepng, 170, 0.7, 0.0, touchbomb, TRUE);
-	DrawRotaGraph((640 - (thousand * 1)) + Movepng, 170, 0.7, 0.0, ignited, TRUE);
-	DrawRotaGraph((640 - (thousand * 2)) + Movepng, 170, 0.7, 0.0, bakuhatu, TRUE);
-	DrawRotaGraph((640 - (thousand * 3)) + Movepng, 170, 0.7, 0.0, be_pursued, TRUE);
-	DrawRotaGraph((640 - (thousand * 4)) + Movepng, 170, 0.7, 0.0, touchsoldier, TRUE);
-	DrawRotaGraph((640 - (thousand * 5)) + Movepng, 170, 0.7, 0.0, chain, TRUE);
+	DrawRotaGraph((980 - (thousand * 0)) + Movepng, 170, 0.7, 0.0, touchbomb, TRUE);
+	DrawRotaGraph((300 - (thousand * 0)) + Movepng, 170, 0.7, 0.0, ignited, TRUE);
+	DrawRotaGraph((980 - (thousand * 1)) + Movepng, 170, 0.7, 0.0, be_pursued, TRUE);
+	DrawRotaGraph((300 - (thousand * 1)) + Movepng, 170, 0.7, 0.0, touchsoldier, TRUE);
+	DrawRotaGraph((640 - (thousand * 2)) + Movepng, 170, 0.7, 0.0, chain, TRUE);
 }
