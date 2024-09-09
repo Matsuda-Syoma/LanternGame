1c16d5225b910c5315071fbca7d7ca1e892bee76
Your Name <aito-maezato-k22f@stu.kbc.ac.jp>
2024-09-09 15:15:24 +0900

    爆弾に触れたときにSEを流す

--- LanturnGame/Object/Bomb.cpp
+++ LanturnGame/Object/Bomb.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 #include "../Scene/GameMain.h"
 #include "CameraManager.h"
+#include "../Utility/LoadSounds.h"
 int Bomb::images[3];
 Bomb::Bomb()
 {
@@ -31,7 +32,11 @@ void Bomb::Hit(Object* _obj)
 		Vector2D vvec = (GetLocation() - _obj->GetLocation());
 		float length = GetLength(_obj->GetLocation());
 		vvec /= length;
-
+		
+		if (CheckSoundMem(Sounds::SE_Hit) == 0)
+		{
+			PlaySoundMem(Sounds::SE_Hit, DX_PLAYTYPE_BACK);
+		}
 		// 点火してないなら
 		if (!GetExpFlg())
 		{
