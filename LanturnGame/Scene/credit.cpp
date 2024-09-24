6928b6392473f7ce895f14dafa6a524ca91051f8
Your Name <aito-maezato-k22f@stu.kbc.ac.jp>
2024-09-24 12:30:29 +0900

    タイトルロゴとクレジットの位置調整

--- LanturnGame/Scene/credit.cpp
+++ LanturnGame/Scene/credit.cpp
@@ -36,10 +36,10 @@ void Credit::Draw() const
 		DrawRotaGraph((SCREEN_WIDTH - 1100) + 56 * i, 150, 1.0, 0.0, alphabetimage[chr], true);
 	}
 
-	SetFontSize(40);
-	DrawString((SCREEN_WIDTH - 1050), 250, "BGM\n・DOVA-SYNDROME\n・なぐもりずの音楽室\n", 0xffffff);
-	DrawString((SCREEN_WIDTH - 1050), 400, "SE\n・効果音ラボ\n・魔王魂\n", 0xffffff);
-	DrawString((SCREEN_WIDTH - 1050), 550, "エフェクト\n・fxmonster", 0xffffff);
+	SetFontSize(60);
+	DrawString((SCREEN_WIDTH - 950), 100, "BGM\n・DOVA-SYNDROME\n・なぐもりずの音楽室\n", 0xffffff);
+	DrawString((SCREEN_WIDTH - 950), 300, "SE\n・効果音ラボ\n・魔王魂\n", 0xffffff);
+	DrawString((SCREEN_WIDTH - 950), 500, "エフェクト\n・fxmonster", 0xffffff);
 
 	/*SetFontSize(40);
 	DrawString((SCREEN_WIDTH - 1060), 400, "", 0xffffff);*/
